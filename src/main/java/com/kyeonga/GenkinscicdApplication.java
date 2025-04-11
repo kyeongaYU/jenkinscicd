02a82f9d5aa422ae9937f0b3607f9b54779a1bbe
lastcoder <ruddk658@naver.com>
2025-04-11 09:29:51 +0900

    first commit

---
+++ src/main/java/com/kyeonga/GenkinscicdApplication.java
@@ -0,0 +1,13 @@
+package com.kyeonga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenkinscicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenkinscicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyeonga/logs/GenkinscicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyeonga.logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenkinscicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
